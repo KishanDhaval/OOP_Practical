03fc14788c2ed6a9d78982f6df91f3619008e453
KishanDhaval <165749171+KishanDhaval@users.noreply.github.com>
2024-04-22 22:11:10 +0530

    Add files via upload

---
+++ Practical8/aboutAccount/Que2.java
@@ -0,0 +1,116 @@
+package Practical8.aboutAccount;
+
+import java.util.*;
+
+interface Branch {
+    void setBranch();
+}
+
+class Account {
+    int Ac_No;
+    String Name;
+    double Balance;
+
+    void display() {
+        System.out.println("Account no. : " + Ac_No);
+        System.out.println("Name : " + Name);
+        System.out.println("Balance : " + Balance);
+    }
+
+    void setData() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Account no. : ");
+        this.Ac_No = sc.nextInt();
+
+        System.out.print("Enter the name : ");
+        this.Name = sc.next();
+
+        System.out.print("Enter the Balance : ");
+        this.Balance = sc.nextDouble();
+    }
+
+    void deposit() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the amount to deposit : ");
+        double Deposit = sc.nextDouble();
+
+        Balance += Deposit;
+    }
+}
+
+class Savings extends Account implements Branch {
+    String BranchName;
+
+    Savings() {
+        System.out.println("Savings account created.");
+    }
+
+    public void setBranch() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Branch Name : ");
+        this.BranchName = sc.next();
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Branch Name: " + BranchName);
+    }
+}
+
+class Current extends Account implements Branch {
+    String BranchName;
+
+    Current() {
+        System.out.println("Current account created.");
+    }
+
+    public void setBranch() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Branch Name : ");
+        this.BranchName = sc.next();
+    }
+
+
+    void display() {
+        super.display();
+        System.out.println("Branch Name: " + BranchName);
+    }
+}
+
+class Que2 {
+    public static void arraylist() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Account> accounts = new ArrayList<>();
+
+        System.out.println("How many Savings Account :");
+        int size = sc.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            Savings s = new Savings();
+            s.setBranch();
+            s.setData();
+            accounts.add(s);
+        }
+
+        System.out.println("How many Current Account :");
+        size = sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            Current c = new Current();
+            c.setBranch();
+            c.setData();
+            accounts.add(c);
+        }
+
+        // Displaying all accounts
+        System.out.println("Accounts Details!!:");
+        for (Account acc : accounts) {
+            acc.display();
+        }
+    }
+
+    public static void main(String[] args) {
+        arraylist();
+    }
+}
---
+++ Practical8/aboutProduct/Que1.java
@@ -0,0 +1,147 @@
+package Practical8.aboutProduct;
+
+import java.util.*;
+
+interface Place{
+	void setPlace();
+}
+
+class Product
+{	
+    String NAME;
+    double MRP;
+    int QUANTITY;
+
+    void display()
+    {
+	System.out.println("Name of product : " + NAME);
+        System.out.println("MRP of product : " + MRP);
+        System.out.println("QUANTITY of product : " + QUANTITY);
+	System.out.println();
+    }
+
+    void setData()
+    {
+        Scanner sc = new Scanner(System.in);
+	
+	System.out.print("Enter the NAME of product : ");
+        this.NAME = sc.nextLine();
+
+        System.out.print("Enter the MRP of product : ");
+        this.MRP = sc.nextDouble();
+
+        System.out.print("Enter the quantity of product : ");
+        this.QUANTITY = sc.nextInt();
+    }
+}
+
+class Crockery extends Product implements Place
+{
+	String place;
+
+    Crockery()
+    {
+        System.out.println("Crokery is added.");
+    }
+	public void setPlace(){
+		  Scanner sc = new Scanner(System.in);
+		System.out.print("Enter place : ");
+      	        this.place = sc.nextLine();	
+	}
+   	 void display() {
+        	super.display();
+        	System.out.println("Place : " + place);
+    	}
+	
+	
+}
+
+class Household extends Product implements Place
+{
+String place;
+    Household()
+    {
+        System.out.println("Household is added.");
+    }
+	public void setPlace(){
+		  Scanner sc = new Scanner(System.in);
+		System.out.print("Enter place : ");
+      	        this.place = sc.nextLine();	
+	}
+   	 void display() {
+       		 super.display();
+        	System.out.println("Place : " + place);
+   	 }
+	
+	
+}
+
+class Food_items extends Product implements Place
+{
+String place;
+    Food_items()
+    {
+        System.out.println("Food_items is added.");
+    }
+	public void setPlace(){
+		 Scanner sc = new Scanner(System.in);
+		System.out.print("Enter place : ");
+      	        this.place = sc.nextLine();	
+	}
+    	 void display() {
+       		 super.display();
+       		 System.out.println("Place : " + place);
+   	 }
+	
+}
+
+
+
+class Que1
+{
+
+	  public static void arraylist() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Product> products = new ArrayList<>();
+
+        System.out.println("How many Crockery Product :");
+        int size = sc.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            Crockery c = new Crockery();
+	    c.setPlace();
+            c.setData();
+            products.add(c);
+        }
+
+        System.out.println("How many House-Hold Product :");
+        size = sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            Household h = new Household();
+	    h.setPlace();
+            h.setData();
+            products.add(h);
+        }
+
+        System.out.println("How many Food-items Product :");
+        size = sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            Food_items f = new Food_items();
+	    f.setPlace();
+            f.setData();
+            products.add(f);
+        }
+
+        // Displaying all accounts
+        System.out.println("Accounts Detail!!:");
+        for (Product pdc : products) {
+            pdc.display();
+        }
+    }
+
+    public static void main(String[] args) {
+        arraylist();
+    }
+
+
+}
